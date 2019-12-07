6dbd03a49ee3f64931c773b4cdd809c0fd138638
ryan1985-xia <58618932+ryan1985-xia@users.noreply.github.com>
2019-12-07 14:53:36 +0800

    Update setenv.sh

--- src/setenv.sh
+++ src/setenv.sh
@@ -1,8 +1,8 @@
 export GITACCOUNT=ryan1985-xia
 
 export MYCLUSTER=tektonknativebeijing08
-export KUBECONFIG=
+export KUBECONFIG=/tmp/ic/AB/.bluemix/plugins/container-service/clusters/tektonknativebeijing08/kube-config-tok04-tektonknativebeijing08.yml
 
 export NAMESPACE=tektondevops-Ryan
 export EMAIL=xiarz@cn.ibm.com
-export INGRESS=
+export INGRESS=tektonknativebeijing08-0e3e0ef4c9c6d831e8aa6fe01f33bfc4-0001.jp-tok.containers.appdomain.cloud
